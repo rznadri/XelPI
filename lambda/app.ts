0705b841a9cc471e237a5fbe0321bb90cff2adc2
Xeleb <rznadri@gmail.com>
2024-12-19 18:06:21 -0500

    update the ts file to fix express issue

--- lambda/app.ts
+++ lambda/app.ts
@@ -1,4 +1,4 @@
-import * as express from "express"
+import express from "express"
 import { RegisterRoutes } from "./routes/routes"; // This is the generated routes file
 
 const app = express();
--- lambda/index.ts
+++ lambda/index.ts
@@ -2,3 +2,7 @@ import serverlessExpress from '@codegenie/serverless-express';
 import app from "./app";
 
 exports.handler = serverlessExpress({ app })
+
+
+
+
